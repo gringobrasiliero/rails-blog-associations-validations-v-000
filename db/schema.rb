b858d047e3a7a739594b9f016f4a0fef1b67242f
Nolan Byrnes <nolanehs@gmail.com>
2018-02-28 10:52:59 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228104259) do
+ActiveRecord::Schema.define(version: 20180228105236) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20180228104259) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
 end
