d6c655cb8a29833b9a0eb552dbfb9b5be98b1cba
Nolan Byrnes <nolanehs@gmail.com>
2018-02-28 11:13:41 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-    validates :name, uniqueness:true
+  has_many :posts
+  has_many :tags, through: :posts
+
+  validates :name, presence: true, uniqueness: true
 end
